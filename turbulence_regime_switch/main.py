6ba1b6639f9bc5bcbd5f534ffd42ec6b48712b8a
Aviv Levinsky <levinskyaviv@gmail.com>
2024-07-07 05:31:18 -0700

    update .toml project name

---
+++ turbulence_regime_switch/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import yahooquery as yq
+import pandas as pd
+
+import plotly.graph_objects as go
+import plotly.express as px
+
+
+pd.options.plotting.backend = 'plotly'
+
+print('test')
+
